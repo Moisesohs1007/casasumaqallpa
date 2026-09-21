59a5b216e8e078d02374ca34657b3f7b2ab5275f
Moisesohs1007 <moisesohs@gmail.com>
2026-09-21 08:07:38 -0500

    Fix CheckinModal 2do error: IonCardTitle + IonSelect/IonSelectOption faltaban en imports. Evita futuros crash ReferenceError encadenados.

--- src/components/modals/CheckinModal.tsx
+++ src/components/modals/CheckinModal.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import {
   IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput,
   IonItem, IonLabel, IonList, IonModal, IonPage, IonRow, IonTextarea, IonTitle, IonToolbar,
-  IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonBadge, IonNote, IonAlert,
+  IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonBadge, IonNote, IonAlert,
+  IonSelect, IonSelectOption,
 } from '@ionic/react';
 import { checkmark, closeOutline, calendar, bed, key, person, cash, pricetags, alertCircle, checkmarkCircle } from 'ionicons/icons';
 import type { Reserva, Huesped, Habitacion, Folio } from '../../types';
